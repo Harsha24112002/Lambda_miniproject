b05184222348717ae9b6a41f58efea340919c78f
Harsha <cs20btech11028@iith.ac.in>
2021-07-03 19:32:04 +0530

    Update Player according to Powerups,added reset

--- Player.cpp
+++ Player.cpp
@@ -1,24 +1,26 @@
 #include "Player.h"
 #include <iostream>
-Player::Player(sf::Texture* t,sf::Color color)
+Player::Player(sf::Texture* t,sf::Color color,sf::Texture* bullettex,sf::Vector2f pos)
 {
+	this->pos=pos;
+
 	body.setTexture(*t);
-	body.setPosition(500,940);
+	body.setPosition(pos);
 	body.setColor(color);
+
+	kill=3;
+	equip(SHIELD);
 	Bulletreltime=0.0f;
-	bullettex.loadFromFile("laserBullet.png");
+	
+	
+	this->color=color;
 	this->movement={0.0f,0.0f};
-	this->velocity=50.0f;
-	this->font.loadFromFile("HOMOARAK.TTF");
-	s="SCORE : ";
-	this->text.setString(s);
-	this->text.setCharacterSize(30);
-	this->text.setPosition(1400,100);
-	this->text.setFillColor(sf::Color::Green);
-	this->text.setFont(font);
+	this->velocity=PVEL;
+	this->bullettex=bullettex;
 	this->score=0;
 	body.setScale(0.2,0.2);
 	body.setOrigin(body.getGlobalBounds().width/2.0,body.getGlobalBounds().height/2);
+	invisible=false;
 }
 Player::~Player()
 {
@@ -39,6 +41,34 @@ bool Player::checkhitenemy(Enemy* enemy)
 	}
 	return false;
 }
+void Player::equip(Powerups type)
+{
+	this->type=type;
+	if(type==SHIELD)
+	{
+	shield = new sf::CircleShape(SHIELDSIZE);
+	shield->setPosition(body.getPosition());
+	shield->setOutlineThickness(10);
+	shield->setOutlineColor(sf::Color::Cyan);
+	shield->setFillColor(sf::Color::Transparent);
+	shieldtime=0;
+	}
+	else if(type==INVISIBILTY)
+	{
+		body.setColor(sf::Color(body.getColor().toInteger()-180));
+		invisible =true;
+		shieldtime=0;
+	}
+	else if(type==MEDICINE)
+	{
+		getheal();
+		shieldtime=0;
+	}
+}
+void Player::getheal()
+{
+	kill=3;
+}
 bool Player::checkhitbullet(Bullet* enemybullet)
 {
 	unsigned index=0;
@@ -57,20 +87,23 @@ bool Player::checkhitbullet(Bullet* enemybullet)
 }
 void Player::createbullet()
 {
-	if(Bulletreltime>TimebetweenBullets ||bullets.size()==0)
+	if(Bulletreltime>TIMEBETWEENBULLETS ||bullets.size()==0)
 	{
 	Bulletreltime=0.0f;
-	bullets.push_back(new Bullet(&bullettex,body.getPosition(),1.0f,sf::Color::Green,10));
+	bullets.push_back(new Bullet(bullettex,body.getPosition(),1.0f,sf::Color::White,BULLETSPEED));
 	}
-}
-void Player::textformat(std::string a,sf::Vector2f pos)
-{
-	s=a;
-	text.setPosition(pos);
 }
 void Player::increasescore()
 {
 	score+=5;
+}
+void Player::reset()
+{
+	score=0;
+	body.setPosition(pos);
+	kill=3;
+	equip(SHIELD);
+	invisible=false;
 }
 void Player::move(sf::Vector2f direction,float dt)
 {
@@ -83,8 +116,29 @@ void Player::update(float dt)
 {
 	
 	Bulletreltime+=dt;
-	text.setString(s+std::to_string(score));
+	shieldtime+=dt;
+	if(shieldtime>SHIELDONTIME)
+	{
+		if(type==0)
+		{
+		if(shield)
+		{
+		shieldtime=0;
+		delete shield;
+		shield =nullptr;
+		}
+		}
+		if(type==1)
+		{
+			invisible=false;
+			body.setColor(color);
+		}
+	}
 	
+	if(shield)
+	{
+		shield->setPosition(body.getPosition().x-20,body.getPosition().y-30);
+	}
 	body.move(movement);
 	updatebullets(dt);
 	for(auto& b:bullets)
@@ -106,9 +160,12 @@ void Player::updatebullets(float dt)
 void Player::render(sf::RenderWindow* window)
 {
 	window->draw(body);
-	window->draw(text);
 	for(auto& a: bullets)
 	{
 		a->render(window);
+	}
+	if(shield)
+	{
+		window->draw(*shield);
 	}
 }
--- Player.h
+++ Player.h
@@ -1,11 +1,16 @@
 #ifndef PLAYER_H
 #define PLAYER_H
-#define TimebetweenBullets 50.0
 #include <SFML/Graphics.hpp>
 #include"Bullet.h"
 #include "Enemy.h"
 #include <vector>
 #include <string>
+#define SHIELDONTIME 5.0
+#define TIMEBETWEENBULLETS 1.45
+#define SHIELDSIZE 70.0
+#define PVEL 500.0
+#define BULLETSPEED 360.0
+enum Powerups {SHIELD=0,INVISIBILTY,MEDICINE};
 class Player
 {
 	private:
@@ -14,27 +19,38 @@ class Player
 	sf::Vector2f movement;
 	unsigned score;	
 	std::vector<Bullet*> bullets;
-	sf::Font font;
-	sf::Text text;
-	sf::Texture bullettex;
+	sf::CircleShape* shield;
+	sf::Vector2f pos;
+	sf::Texture* bullettex;
 	float Bulletreltime;
-	unsigned count=0;
-	std::string s;
-
+	float shieldtime;
+	unsigned kill;
+	sf::Color color;
+	Powerups type;
+	bool invisible;
 	public:
-	Player(sf::Texture* t,sf::Color color);
+	Player(sf::Texture* t,sf::Color color,sf::Texture* bullettex,sf::Vector2f pos);
 	~Player();
-	void move(sf::Vector2f direction,float dt);
+
+	void incrementkill(){kill--;}
+	unsigned getkill(){return kill;}
+	void move(sf::Vector2f direction, float dt);
 	void update(float dt);
 	void render(sf::RenderWindow* window);
 	void increasescore();
-	void textformat(std::string a,sf::Vector2f pos);
+	sf::Color getcolor(){return body.getColor();}
+	unsigned getscore(){return score;}
 	bool checkhitenemy(Enemy* enemy);
 	bool checkhitbullet(Bullet* bullet);
+	bool hasshield(){return shield;}
+	void equip(Powerups type);
+	void getheal();
+	void reset();
+	sf::Vector2f getpos(){return body.getPosition();}	
 	void setpos(sf::Vector2f pos){body.setPosition(pos);}
 	void updatebullets(float dt);
 	void createbullet();
-	sf::Vector2f getpos(){return body.getPosition();}
+	bool isinvisible(){return invisible;}
 	Collider getcollider(){return Collider(body);}
 	
 };
